7eb500d555a6cc5e9a66fcfbf6ec8ec988ea46f9
steven-yanh <hyan0820@gmail.com>
2022-10-03 01:02:10 -0400

    TicTacToe: setup score header

--- BoardGames/BoardGames/TicTacToeViewController.swift
+++ BoardGames/BoardGames/TicTacToeViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class TicTacToeViewController: UIViewController {
+    //title components
+    let XLabel = UILabel()
+    let scoreLabel = UILabel()
+    let turnArrow = UIImageView()
+    let OLabel = UILabel()
+    var xScoreInt = 0 {
+        didSet {
+            scoreLabel.text = String(format: "%1d : %1d", arguments: [xScoreInt,oScoreInt])
+        }
+    }
+    var oScoreInt = 0 {
+        didSet {
+            scoreLabel.text = String(format: "%1d : %1d", arguments: [xScoreInt,oScoreInt])
+        }
+    }
     
+    
+    
+    //board components & logic
     let VStack = UIStackView()
     let HStackA = UIStackView()
     let HStackB = UIStackView()
@@ -28,7 +46,43 @@ class TicTacToeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTitle()
         setupBoard()
+    }
+    func setupTitle() {
+        view.addSubview(XLabel)
+        view.addSubview(turnArrow)
+        view.addSubview(scoreLabel)
+        view.addSubview(OLabel)
+        view.addSubview(scoreLabel)
+        
+        XLabel.translatesAutoresizingMaskIntoConstraints = false
+        turnArrow.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        OLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        XLabel.font = UIFont.systemFont(ofSize: 90)
+        XLabel.text = "X"
+        OLabel.font = UIFont.systemFont(ofSize: 90)
+        OLabel.text = "O"
+        turnArrow.image = UIImage(systemName: "arrowtriangle.up.fill")
+        turnArrow.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        turnArrow.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        scoreLabel.font = UIFont.systemFont(ofSize: 50)
+        xScoreInt = 0 // triggers didset to show scoreLabel
+        
+        NSLayoutConstraint.activate([
+            XLabel.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 3),
+            XLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 3),
+            OLabel.topAnchor.constraint(equalTo: XLabel.topAnchor),
+            OLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            scoreLabel.topAnchor.constraint(equalToSystemSpacingBelow: XLabel.topAnchor, multiplier: 3),
+            scoreLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            turnArrow.topAnchor.constraint(equalToSystemSpacingBelow: XLabel.bottomAnchor, multiplier: 1),
+            turnArrow.leadingAnchor.constraint(equalToSystemSpacingAfter: XLabel.leadingAnchor, multiplier: 1)
+        ])
+        
     }
     func setupBoard() {
         view.addSubview(VStack)
@@ -79,7 +133,7 @@ class TicTacToeViewController: UIViewController {
             VStack.widthAnchor.constraint(equalToConstant: width),
             VStack.heightAnchor.constraint(equalToConstant: width),
             VStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            VStack.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            VStack.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: +70)
         ])
         
         
@@ -89,7 +143,7 @@ class TicTacToeViewController: UIViewController {
         button.setTitle(" ", for: [])
         button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         button.backgroundColor = .systemBackground
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 50)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 70)
         button.titleLabel?.adjustsFontSizeToFitWidth = true
         button.translatesAutoresizingMaskIntoConstraints = false
         
@@ -113,5 +167,11 @@ extension TicTacToeViewController {
         } else {
             playerSign = "X"
         }
+    }
+}
+//MARK: - ChekingWinner && pop up alert
+extension TicTacToeViewController {
+    func checkIfWin() {
+        
     }
 }
