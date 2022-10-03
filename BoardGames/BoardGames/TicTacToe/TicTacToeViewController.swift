b76d6db0c991d5db7ad7fd913f1ebc66bac782c7
steven-yanh <hyan0820@gmail.com>
2022-10-03 11:38:33 -0400

    TicTacToe: checking win logic

--- BoardGames/BoardGames/TicTacToe/TicTacToeViewController.swift
+++ BoardGames/BoardGames/TicTacToe/TicTacToeViewController.swift
@@ -32,15 +32,15 @@ class TicTacToeViewController: UIViewController {
     let HStackB = UIStackView()
     let HStackC = UIStackView()
     
-    lazy var a1: UIButton = { return makeButton() }()
-    lazy var a2: UIButton = { return makeButton() }()
-    lazy var a3: UIButton = { return makeButton() }()
-    lazy var b1: UIButton = { return makeButton() }()
-    lazy var b2: UIButton = { return makeButton() }()
-    lazy var b3: UIButton = { return makeButton() }()
-    lazy var c1: UIButton = { return makeButton() }()
-    lazy var c2: UIButton = { return makeButton() }()
-    lazy var c3: UIButton = { return makeButton() }()
+    lazy var i0: UIButton = { return makeButton() }()
+    lazy var i1: UIButton = { return makeButton() }()
+    lazy var i2: UIButton = { return makeButton() }()
+    lazy var i3: UIButton = { return makeButton() }()
+    lazy var i4: UIButton = { return makeButton() }()
+    lazy var i5: UIButton = { return makeButton() }()
+    lazy var i6: UIButton = { return makeButton() }()
+    lazy var i7: UIButton = { return makeButton() }()
+    lazy var i8: UIButton = { return makeButton() }()
     
     var playerSign = "X"
     
@@ -72,6 +72,7 @@ class TicTacToeViewController: UIViewController {
         scoreLabel.font = UIFont.systemFont(ofSize: 50)
         xScoreInt = 0 // triggers didset to show scoreLabel
         
+        
         NSLayoutConstraint.activate([
             XLabel.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 3),
             XLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 3),
@@ -87,31 +88,31 @@ class TicTacToeViewController: UIViewController {
     func setupBoard() {
         view.addSubview(VStack)
         VStack.addArrangedSubview(HStackA)
-        HStackA.addArrangedSubview(a1)
-        HStackA.addArrangedSubview(a2)
-        HStackA.addArrangedSubview(a3)
+        HStackA.addArrangedSubview(i0)
+        HStackA.addArrangedSubview(i1)
+        HStackA.addArrangedSubview(i2)
         VStack.addArrangedSubview(HStackB)
-        HStackB.addArrangedSubview(b1)
-        HStackB.addArrangedSubview(b2)
-        HStackB.addArrangedSubview(b3)
+        HStackB.addArrangedSubview(i3)
+        HStackB.addArrangedSubview(i4)
+        HStackB.addArrangedSubview(i5)
         VStack.addArrangedSubview(HStackC)
-        HStackC.addArrangedSubview(c1)
-        HStackC.addArrangedSubview(c2)
-        HStackC.addArrangedSubview(c3)
+        HStackC.addArrangedSubview(i6)
+        HStackC.addArrangedSubview(i7)
+        HStackC.addArrangedSubview(i8)
         
         VStack.translatesAutoresizingMaskIntoConstraints = false
         HStackA.translatesAutoresizingMaskIntoConstraints = false
         HStackB.translatesAutoresizingMaskIntoConstraints = false
         HStackC.translatesAutoresizingMaskIntoConstraints = false
-        a1.translatesAutoresizingMaskIntoConstraints = false
-        a2.translatesAutoresizingMaskIntoConstraints = false
-        a3.translatesAutoresizingMaskIntoConstraints = false
-        b1.translatesAutoresizingMaskIntoConstraints = false
-        b2.translatesAutoresizingMaskIntoConstraints = false
-        b3.translatesAutoresizingMaskIntoConstraints = false
-        c1.translatesAutoresizingMaskIntoConstraints = false
-        c2.translatesAutoresizingMaskIntoConstraints = false
-        c3.translatesAutoresizingMaskIntoConstraints = false
+        i0.translatesAutoresizingMaskIntoConstraints = false
+        i1.translatesAutoresizingMaskIntoConstraints = false
+        i2.translatesAutoresizingMaskIntoConstraints = false
+        i3.translatesAutoresizingMaskIntoConstraints = false
+        i4.translatesAutoresizingMaskIntoConstraints = false
+        i5.translatesAutoresizingMaskIntoConstraints = false
+        i6.translatesAutoresizingMaskIntoConstraints = false
+        i7.translatesAutoresizingMaskIntoConstraints = false
+        i8.translatesAutoresizingMaskIntoConstraints = false
         
         VStack.axis = .vertical
         VStack.spacing = 8
@@ -140,7 +141,7 @@ class TicTacToeViewController: UIViewController {
     }
     func makeButton() -> UIButton {
         let button = UIButton(type: .system)
-        button.setTitle(" ", for: [])
+        button.setTitle(" ", for: .normal)
         button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         button.backgroundColor = .systemBackground
         button.titleLabel?.font = UIFont.systemFont(ofSize: 70)
@@ -157,9 +158,11 @@ extension TicTacToeViewController {
     @objc func buttonTapped(_ sender: UIButton) {
         guard let text = sender.titleLabel?.text else { return }
         if text == " " {
-            sender.setTitle(playerSign, for: [])
-            switchSide()
+            sender.titleLabel?.text = playerSign
+            sender.setTitle(playerSign, for: .normal)
         }
+        checkIfWin()
+        switchSide()
     }
     func switchSide() {
         if playerSign == "X" {
@@ -172,6 +175,55 @@ extension TicTacToeViewController {
 //MARK: - ChekingWinner && pop up alert
 extension TicTacToeViewController {
     func checkIfWin() {
-        
+        let boardString = makeArr()
+        if checkHorizontal(from: 0, boardString) || checkHorizontal(from: 3, boardString) || checkHorizontal(from: 6, boardString) {
+            print("win")
+        }
+        if checkVertical(from: 0, boardString) || checkVertical(from: 1, boardString) || checkVertical(from: 2, boardString) {
+            print("Vwin")
+        }
+        if checkDiagonal(boardString) {
+            print("dWin")
+        }
+    }
+    func makeArr() -> [String] {
+        var result = [String]()
+        result.append(i0.getText())
+        result.append(i1.getText())
+        result.append(i2.getText())
+        result.append(i3.getText())
+        result.append(i4.getText())
+        result.append(i5.getText())
+        result.append(i6.getText())
+        result.append(i7.getText())
+        result.append(i8.getText())
+        return result
+    }
+    func checkHorizontal(from index:Int, _ boardString: [String]) -> Bool {
+        if boardString[index] == boardString[index+1] && boardString[index+1] == boardString[index+2] && boardString[index] != " " {
+            return true
+        }
+        return false
+    }
+    func checkVertical(from index: Int, _ boardString: [String]) -> Bool {
+        if boardString[index] == boardString[index+3] && boardString[index+3] == boardString[index+6] && boardString[index] != " " {
+            return true
+        }
+        return false
+    }
+    func checkDiagonal(_ boardString: [String]) -> Bool {
+        if boardString[0] == boardString[4] && boardString[4] == boardString[8] && boardString[0] != " " {
+            return true
+        }
+        if boardString[2] == boardString[4] && boardString[4] == boardString[6] && boardString[2] != " " {
+            return true
+        }
+        return false
+    }
+}
+
+extension UIButton {
+    func getText() -> String {
+        return titleLabel?.text ?? ""
     }
 }
